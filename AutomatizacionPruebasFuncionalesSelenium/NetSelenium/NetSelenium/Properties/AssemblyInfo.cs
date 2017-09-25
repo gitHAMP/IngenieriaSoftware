7232196e847024d029c7d3e02870afb1970bbf6c
Henry A. Mendoza Puerta <hmendo81@gmail.com>
2017-09-25 11:20:50 -0500

    AutomatizacionPruebas-Selenium

---
+++ AutomatizacionPruebasFuncionalesSelenium/NetSelenium/NetSelenium/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NetSelenium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NetSelenium")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ba668a9e-a0e7-4a38-a8b2-f7e19653eacd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AutomatizacionPruebasFuncionalesSelenium/NetSelenium/NetSelenium/UnitTest1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+using System.Threading;
+
+namespace NetSelenium
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        //    driver.Navigate().GoToUrl("http://www.swtestacademy.com/");
+        //    Assert.AreEqual("Software Test Academy | Software Testing Academy", driver.Title);
+        //    driver.Close();
+        //    driver.Quit();
+        }
+
+        [TestMethod]
+        public void insertarCategoria_FlujoBasico()
+        {
+            try
+            {
+                string mensajeEsperado = "Se guardó de manera correcta la Categoría";
+                driver.Navigate().GoToUrl("http://localhost:8180/TuBodeguitaWeb/");
+
+                IWebElement input1 = driver.FindElement(By.Id("txtUsuario"));
+                input1.Clear();
+                input1.SendKeys("admin");
+                IWebElement input2 = driver.FindElement(By.Id("txtClave"));
+                input2.Clear();
+                input2.SendKeys("clave");
+                IWebElement btnIniciaSesion = driver.FindElement(By.Id("btnIniciarSesion"));
+                btnIniciaSesion.Click();
+                Thread.Sleep(2000);
+
+                IWebElement opcion = driver.FindElement(By.XPath("//*[@id=\"frmMenu:j_idt18\"]/ul/li[2]/a"));
+                opcion.Click();
+                Thread.Sleep(2000);
+
+
+                IWebElement btnNuevo = driver.FindElement(By.Id("btnNuevo"));
+                btnNuevo.Click();
+
+                IWebElement input3 = driver.FindElement(By.Id("txtNombre"));
+                input3.Clear();
+                input3.SendKeys("Categoria Prueba");
+                Thread.Sleep(2000);
+
+                IWebElement btnGuardar = driver.FindElement(By.Id("btnGuardar"));
+                btnGuardar.Click();
+                Thread.Sleep(2000);
+
+                string mensajeObtenido = driver.FindElement(By.Id("messages")).Text;
+                Assert.AreEqual(mensajeEsperado, mensajeObtenido);
+                driver.Close();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail();
+
+            }
+        }
+
+        [TestMethod]
+        public void insertarCategoria_FlujoAlternativo()
+        {
+            try
+            {
+                string mensajeEsperado = "Nombre: Error de validación: se necesita un valor.";
+                driver.Navigate().GoToUrl("http://localhost:8180/TuBodeguitaWeb/");
+
+                IWebElement input1 = driver.FindElement(By.Id("txtUsuario"));
+                input1.Clear();
+                input1.SendKeys("admin");
+                IWebElement input2 = driver.FindElement(By.Id("txtClave"));
+                input2.Clear();
+                input2.SendKeys("clave");
+                IWebElement btnIniciaSesion = driver.FindElement(By.Id("btnIniciarSesion"));
+                btnIniciaSesion.Click();
+                Thread.Sleep(2000);
+
+                IWebElement opcion = driver.FindElement(By.XPath("//*[@id=\"frmMenu:j_idt18\"]/ul/li[2]/a"));
+                opcion.Click();
+                Thread.Sleep(2000);
+
+
+                IWebElement btnNuevo = driver.FindElement(By.Id("btnNuevo"));
+                btnNuevo.Click();
+
+                IWebElement input3 = driver.FindElement(By.Id("txtNombre"));
+                input3.Clear();
+                input3.SendKeys("");
+                Thread.Sleep(2000);
+
+                IWebElement btnGuardar = driver.FindElement(By.Id("btnGuardar"));
+                btnGuardar.Click();
+                Thread.Sleep(2000);
+
+                string mensajeObtenido = driver.FindElement(By.Id("messages")).Text;
+                Assert.AreEqual(mensajeEsperado, mensajeObtenido);
+                driver.Close();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail();
+
+            }
+
+
+
+        }
+    }
+}
